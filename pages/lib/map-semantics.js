a98caa21c43bdad800ec885fb57f790093a03567
Nick Freear <nfreear@yahoo.co.uk>
2025-05-02 21:31:44 +0100

    Replace the map tile image - using 1864 Mitchell Map of the World
    * https://commons.wikimedia.org/wiki/File:1864_Mitchell_Map_of_the_World_on_Mercator_Projection_-_Geographicus_-_World-mitchell-1864.jpg

--- pages/lib/map-semantics.js
+++ pages/lib/map-semantics.js
@@ -5,22 +5,24 @@ const mapRegion = document.querySelector('#mapRegion');
 const panButton = mapRegion.querySelector('#panButton');
 const controlsGroup = mapRegion.querySelector('#controlsGroup');
 const pinsGroup = mapRegion.querySelector('#pinsGroup');
-const pinsDialog = mapRegion.querySelector('dialog');
-const dlgOutput = pinsDialog.querySelector('output');
+const mapDialog = mapRegion.querySelector('dialog');
+const dlgOutput = mapDialog.querySelector('output');
 
 pinsGroup.addEventListener('click', (ev) => {
   ev.preventDefault();
-  if (ev.target.tagName === 'BUTTON') {
+  if (hasButton(ev)) {
     dlgOutput.value = ev.target.textContent;
-    pinsDialog.showModal();
+    mapDialog.showModal();
   }
+
+  console.debug('Pin click:', ev);
 });
 
 controlsGroup.addEventListener('click', (ev) => {
   ev.preventDefault();
-  if (ev.target.tagName === 'BUTTON') {
+  if (hasButton(ev)) {
     dlgOutput.value = ev.target.textContent;
-    pinsDialog.showModal();
+    mapDialog.showModal();
   }
 });
 
@@ -38,3 +40,8 @@ mapStyle.addEventListener('change', (ev) => {
 });
 
 document.body.dataset.mapStyle = true;
+
+function hasButton (event) {
+  // Was: if(ev.target.tagName === 'BUTTON')
+  return event.target.closest('button');
+}
