ffc5569d82f7a014836621c58b4549bfbb1152e5
Nick Freear <nfreear@yahoo.co.uk>
2025-05-08 19:40:51 +0100

    page: Updates to `map-semantics` HTML, JavaScript and CSS

--- pages/lib/map-semantics.js
+++ pages/lib/map-semantics.js
@@ -7,23 +7,36 @@ const controlsGroup = mapRegion.querySelector('#controlsGroup');
 const pinsGroup = mapRegion.querySelector('#pinsGroup');
 const mapDialog = mapRegion.querySelector('dialog');
 const dlgOutput = mapDialog.querySelector('output');
+const dlgHeading = mapDialog.querySelector('h2, h3');
 
 pinsGroup.addEventListener('click', (ev) => {
-  ev.preventDefault();
-  if (hasButton(ev)) {
-    dlgOutput.value = ev.target.textContent;
-    mapDialog.showModal();
-  }
+  const label = ev.target.textContent;
 
-  console.debug('Pin click:', ev);
+  if (hasButtonIsh(ev)) {
+    dlgHeading.textContent = label;
+    dlgOutput.value = pinContent(ev);
+
+    if (hasStyle()) {
+      mapDialog.showModal();
+      ev.preventDefault();
+    }
+    else if (hasButton(ev)) {
+      mapDialog.showModal();
+    }
+  }
+  console.debug('Pin click:', label, hasStyle(), ev);
 });
 
 controlsGroup.addEventListener('click', (ev) => {
   ev.preventDefault();
+
+  const label = ev.target.textContent;
+
   if (hasButton(ev)) {
     dlgOutput.value = ev.target.textContent;
     mapDialog.showModal();
   }
+  console.debug('Control click:', label, ev);
 });
 
 panButton.addEventListener('keyup', (ev) => {
@@ -32,7 +45,7 @@ panButton.addEventListener('keyup', (ev) => {
   }
 });
 
-console.debug('Pins:', pinsGroup.elements);
+console.debug('Pins:', pinsGroup.querySelectorAll('button, summary'));
 console.debug('Controls:', controlsGroup.elements);
 
 mapStyle.addEventListener('change', (ev) => {
@@ -41,7 +54,25 @@ mapStyle.addEventListener('change', (ev) => {
 
 document.body.dataset.mapStyle = true;
 
+function hasStyle () {
+  return document.body.dataset.mapStyle !== 'false';
+}
+
+function hasButtonIsh (event) {
+  return event.target.closest('button') || event.target.closest('summary');
+}
+
 function hasButton (event) {
   // Was: if(ev.target.tagName === 'BUTTON')
   return event.target.closest('button');
+}
+
+function pinContent (event) {
+  if (hasButton(event)) {
+    return event.target.textContent; // return early.
+  }
+  const detailsElem = event.target.closest('details');
+  const detailsBody = detailsElem.querySelector('& > :not(summary)');
+  console.assert(detailsBody, '<details> body should exist;')
+  return detailsBody.textContent;
 }
